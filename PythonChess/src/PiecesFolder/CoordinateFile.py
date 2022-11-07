87ee27571ef4b20fae071f10b5b97d46a7bdacea
Eduardo <edward.steinmann@outlook.com>
2022-11-06 20:08:11 -0500

    Made some naming changes to make everything clearer for imports.

--- PythonChess/src/PiecesFolder/CoordinateFile.py
+++ PythonChess/src/PiecesFolder/CoordinateFile.py
@@ -1,5 +1,5 @@
 
-class Position:
+class Coordinate:
     
     def __init__(self, x: int, y: int):
         self.x = x
--- PythonChess/src/PiecesFolder/PiecesFile.py
+++ PythonChess/src/PiecesFolder/PiecesFile.py
@@ -1,9 +1,9 @@
-import Position as Pos
+from CoordinateFile import Coordinate
 from abc import ABC, abstractmethod
 
 class Piece(ABC):
-    def __init__(self, pos: Pos.Position, color_num: int):
-        self.position = pos
+    def __init__(self, pos: Coordinate, color_num: int):
+        self.pos = pos
         self.colorNum = color_num
         if color_num == 0:
             color: chr = 'w'
@@ -15,42 +15,42 @@ class Piece(ABC):
         pass
 
 class Knight(Piece):
-    def __init__(self, pos: Pos.Position, color_num: int):
+    def __init__(self, pos: Coordinate, color_num: int):
         super().__init__(pos, color_num)
     
     def calculate_legal_moves(self):
         legal_moves = []
 
 class Bishop(Piece):
-    def __init__(self, pos: Pos.Position, color_num: int):
+    def __init__(self, pos: Coordinate, color_num: int):
         super().__init__(pos, color_num)
     
     def calculate_legal_moves(self):
         legal_moves = []
 
 class Rook(Piece):
-    def __init__(self, pos: Pos.Position, color_num: int):
+    def __init__(self, pos: Coordinate, color_num: int):
         super().__init__(pos, color_num)
     
     def calculate_legal_moves(self):
         legal_moves = []
 
 class Queen(Piece):
-    def __init__(self, pos: Pos.Position, color_num: int):
+    def __init__(self, pos: Coordinate, color_num: int):
         super().__init__(pos, color_num)
     
     def calculate_legal_moves(self):
         legal_moves = []
 
 class King(Piece):
-    def __init__(self, pos: Pos.Position, color_num: int):
+    def __init__(self, pos: Coordinate, color_num: int):
         super().__init__(pos, color_num)
     
     def calculate_legal_moves(self):
         legal_moves = []
 
 class Pawn(Piece):
-    def __init__(self, pos: Pos.Position, color_num: int):
+    def __init__(self, pos: Coordinate, color_num: int):
         super().__init__(pos, color_num)
     
     def calculate_legal_moves(self):
